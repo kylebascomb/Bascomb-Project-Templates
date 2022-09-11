766a8b42da6d5f08ffdb057037444bef7280d579
Kyle <kylebascomb@gmail.com>
2022-09-10 23:05:48 -0700

    Added Parameterized Test

--- poetry-template/tests/test_even_odd.py
+++ poetry-template/tests/test_even_odd.py
@@ -1,16 +1,10 @@
 from poetry_template.even_odd import EvenOddNumber
+import pytest
 
 
-
-@pytest.mark.parameterize("test_input, expected", [
-    (10, True),
-    (11, False),
-    (0, True),
-    (-1, False),
-    (-2, True)
-
-]
-
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [(10, True), (11, False), (0, True), (-1, False), (-2, True)],
 )
 def test_even_odd(test_input, expected):
     number = EvenOddNumber(test_input)
